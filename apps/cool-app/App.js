3d20774c4a30b50a9c8da69b9d1b4243fbfd19b4
Iuri Pereira <iuricmp@gmail.com>
2023-06-26 14:47:53 +0100

    update lib
    Signed-off-by: Iuri Pereira <iuricmp@gmail.com>

--- apps/cool-app/App.js
+++ apps/cool-app/App.js
@@ -5,7 +5,7 @@ import * as Settings from 'expo-settings';
 export default function App() {
   return (
     <View style={styles.container}>
-			<Text>Theme: {Settings.getTheme()}</Text>
+			<Text>{Settings.hello()}</Text>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
