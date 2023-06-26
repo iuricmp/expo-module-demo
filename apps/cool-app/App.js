12c5228086f6f4b5ef5a44e7147159564aeb508c
Iuri Pereira <iuricmp@gmail.com>
2023-06-26 12:22:28 +0100

    using the module inside the app
    Signed-off-by: Iuri Pereira <iuricmp@gmail.com>

--- apps/cool-app/App.js
+++ apps/cool-app/App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Settings from 'expo-settings';
 
 export default function App() {
   return (
     <View style={styles.container}>
+			<Text>Theme: {Settings.getTheme()}</Text>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
